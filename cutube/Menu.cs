f0e8401852d678505bad1356259b1544ee3d7baf
Wilson Santos <contato@wilsonsantos.net>
2023-08-16 17:48:37 -0300

    refactor: Add algumas validações simples

--- cutube/Menu.cs
+++ cutube/Menu.cs
@@ -3,9 +3,9 @@ namespace cutube;
 
 public static class Menu
 {
-    public static string Url { get; set; } = String.Empty;
-    public static string Start { get; set; } = String.Empty;
-    public static string End { get; set; } = String.Empty;
+    public static string Url { get; private set; } = string.Empty;
+    public static string Start { get; private set; } = string.Empty;
+    public static string End { get; private set; } = string.Empty;
 
     public static void Show()
     {
@@ -13,11 +13,12 @@ public static class Menu
         Console.WriteLine("Desenvolvido por: Wilson Santos");
         Console.WriteLine("-----------------------------------------------");
         Console.WriteLine("1 - Digite a url do vídeo.");
-        Url = Console.ReadLine();
+        Url =  Console.ReadLine() ?? throw new InvalidOperationException("A url não pode ser vazia.");
         Console.WriteLine("2 - Digite o tempo de início (hh:mm:ss).");
-        Start = Console.ReadLine();
+        Start = Console.ReadLine() ?? throw new InvalidOperationException("O tempo de inicio não pode ser vazio.");
         Console.WriteLine("3 - Digite o tempo de fim (hh:mm:ss).");
-        End = Console.ReadLine();
-
+        End = Console.ReadLine() ?? throw new InvalidOperationException("O tempo de fim não pode ser vazio.");
+        
+        
     }
 }
