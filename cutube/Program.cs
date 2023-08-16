80c0b937259792ef5ff184ad5275269639b2c349
Wilson Santos <contato@wilsonsantos.net>
2023-08-16 09:47:14 -0300

    Inicialização basica consegui pegar o titulo de um video estatico, vou trabalhar com ele, antes de incrementar

---
+++ cutube/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using YoutubeExplode;
+
+var youtube = new YoutubeClient();
+var videoUrl = "https://www.youtube.com/watch?v=oKmWXD2i3y8";
+var video = await youtube.Videos.GetAsync(videoUrl);
+
+Console.WriteLine(video.Title);
