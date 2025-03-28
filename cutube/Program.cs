795dc0b97d6d66aceeff11bbc4f2407091b17a8d
Wilson Santos <contato@wilsonsantos.net>
2025-03-28 19:14:14 -0300

    fix: baixa audio e video separado para evitar bug do getmuxed em videos do yt

--- cutube/Program.cs
+++ cutube/Program.cs
@@ -14,54 +14,80 @@ var timeStart = TimeHelper.GetStartSeconds(videoStart);
 var timeEnd = TimeHelper.GetEndSeconds(videoEnd);
 
 var video = await youtube.Videos.GetAsync(videoUrl);
-
-var videoTitle = video.Title;
+var videoTitle = TitleHelper.FormatTitle(video.Title);
 
 var streamManifest =
     await youtube.Videos.Streams.GetManifestAsync(video.Id);
 
-var streamInfo = streamManifest
+var videoStreamInfo = streamManifest
     .GetVideoStreams()
     .TryGetWithHighestVideoQuality();
 
-if (streamInfo is null)
+var audioStreamInfo = streamManifest
+    .GetAudioStreams()
+    .TryGetWithHighestBitrate();
+
+if (videoStreamInfo is null)
 {
     Console.WriteLine("Não foi possível encontrar o video");
     return;
 }
 
-var temp = Path.GetTempFileName();
-
-if (streamInfo == null)
-    throw new Exception("Falha ao baixar o video");
+var tempVideo = Path.GetTempFileName();
+var tempAudio = Path.GetTempFileName();
 
 Console.WriteLine("Iniciando o download do video...");
 Console.WriteLine("O tempo de espera pode variar de acordo com a sua conexão.");
+await youtube.Videos.Streams.DownloadAsync(videoStreamInfo, tempVideo);
+Console.WriteLine("Video baixado com sucesso");
 
-await youtube.Videos.Streams.DownloadAsync(streamInfo, temp);
+if (audioStreamInfo is not null)
+{
+    Console.WriteLine("Iniciando o download do áudio...");
+    await youtube.Videos.Streams.DownloadAsync(audioStreamInfo, tempAudio);
+    Console.WriteLine("Áudio baixado com sucesso");
+}
+else
+{
+    Console.WriteLine("Não foi possível encontrar o áudio");
+}
 
 Console.WriteLine("Video baixado com sucesso");
 
-videoTitle = TitleHelper.FormatTitle(videoTitle);
 var output = $"{videoTitle}.mp4";
-
 var ffmpeg = new FfmpegHelper();
 
-var arguments =
-    $"-loglevel verbose -ss {timeStart} -i \"{temp}\" -t {timeEnd - timeStart} -c:v libx264 \"{output}\"";
+string arguments;
+
+if (audioStreamInfo is not null)
+{
+    arguments =
+        $"-loglevel verbose -i \"{tempVideo}\" -i \"{tempAudio}\"  -ss {timeStart} " +
+        $"-t {timeEnd - timeStart} -c:v libx264 -c:a aac -strict experimental \"{output}\"";
+}
+else
+{
+    arguments =
+        $"-loglevel verbose -i \"{tempVideo}\" -ss {timeStart}  " +
+        $"-t {timeEnd - timeStart} -c:v libx264 -an \"{output}\"";
+}
 
 try
 {
     Console.WriteLine("Iniciando o corte do video...");
     Console.WriteLine("Esse processo pode demorar,aguarde...");
     ffmpeg.ExecuteFfmpeg(arguments, new ProgressBar());
-    File.Delete(temp);
+    File.Delete(tempVideo);
+    if (File.Exists(tempAudio))
+        File.Delete(tempAudio);
 }
 catch (Exception e)
 {
     Console.WriteLine(e);
     throw;
 }
-
-Console.WriteLine(
+finally
+{
+    Console.WriteLine(
     $"O video {videoTitle} foi baixado e cortado, o resultado está em: {Path.GetFullPath(output)}");
+}
